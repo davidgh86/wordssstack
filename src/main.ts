4f61055976f7eb27bd5c2e8cea5a2e6ae37256b4
David <david_gh86@hotmail.es>
2022-07-29 22:34:27 +0200

    read filesystem

--- src/main.ts
+++ src/main.ts
@@ -21,6 +21,8 @@ import '@ionic/vue/css/flex-utils.css';
 import '@ionic/vue/css/display.css';
 import * as intentInstance from 'cordova-plugin-intent/www/android/IntentPlugin';
 
+//import { Filesystem, Directory, Encoding } from '@capacitor/filesystem';
+
 /* Theme variables */
 import './theme/variables.css';
 
@@ -44,3 +46,41 @@ document.addEventListener('deviceReady', () => {
   })
 })
 
+// SEE DOCS https://www.npmjs.com/package/@capacitor/filesystem/v/1.1.0
+// const writeSecretFile = async () => {
+//   await Filesystem.writeFile({
+//     path: 'secrets/text.txt',
+//     data: "This is a test",
+//     directory: Directory.Documents,
+//     encoding: Encoding.UTF8,
+//   });
+// };
+
+// const readSecretFile = async () => {
+//   const contents = await Filesystem.readFile({
+//     path: 'secrets/text.txt',
+//     directory: Directory.Documents,
+//     encoding: Encoding.UTF8,
+//   });
+
+//   console.log('secrets:', contents);
+// };
+
+// const deleteSecretFile = async () => {
+//   await Filesystem.deleteFile({
+//     path: 'secrets/text.txt',
+//     directory: Directory.Documents,
+//   });
+// };
+
+// const readFilePath = async () => {
+//   // Here's an example of reading a file with a full file path. Use this to
+//   // read binary data (base64 encoded) from plugins that return File URIs, such as
+//   // the Camera.
+//   const contents = await Filesystem.readFile({
+//     //path: 'file:///var/mobile/Containers/Data/Application/22A433FD-D82D-4989-8BE6-9FC49DEA20BB/Documents/text.txt'
+//     path: 'content:///var/mobile/Containers/Data/Application/22A433FD-D82D-4989-8BE6-9FC49DEA20BB/Documents/text.txt'
+//   });
+
+//   console.log('data:', contents);
+// };
